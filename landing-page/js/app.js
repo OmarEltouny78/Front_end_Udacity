b9e7b2a7e3dee428c2246cabb4cf241d62bbe34f
OmarEltouny78 <omar.eltouny@uofcanada.edu.eg>
2021-03-23 16:25:21 +0200

    SubmissionForReview

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,93 +1,97 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
 /**
  * Define Global Variables
  * 
 */
 
+
+let unorderedList = document.getElementById("unordered-list");
+let containerCount = document.getElementsByClassName("landing__container")
+  .length;
+
 /**
  * End Global Variables
- * Start Helper Functions
  * 
 */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-function sectionswtich(id,min,max,a){
-  const element= document.getElementById(id);
-  const active=document.getElementById(a);
-    if (document.body.scrollTop > min && document.body.scrollTop < max || document.documentElement.scrollTop > min &&document.documentElement.scrollTop < max) {
-        element.className = "your-active-class";
-        active.className="active";
-        
-      } else {
-        element.className = "";
-        active.className="";
-
-      }
-
-}
 
 // build the nav
+for (i = 1; i < containerCount + 1; i++) {
+  let item = "#section" + i + "-title";
+  let itemValue = document.querySelector(item);
+  let itemText = itemValue.textContent;
+  let newLine = document.createElement("li");
+  let lineText = document.createTextNode(itemText);
+  let listItem = "sample-nav-" + i;
+  newLine.setAttribute("id", listItem);
+  newLine.setAttribute("class", "nav-list-item");
+  newLine.appendChild(lineText);
+  unorderedList.appendChild(newLine);
+
+  let itemTarget = document.getElementById("section" + i);
+  let listTarget = document.getElementById(listItem);
+  let buttonName = "section" + i + "-button";
+  let buttonToAdd = document.getElementById(buttonName);
+
+  //Adds button when scrolls into view
+
+  listTarget.addEventListener("click", function() {
+    itemTarget.scrollIntoView ({
+      behavior: 'smooth'}
+    )
+
+  });
+}
 
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-
-
+function sectionSwtich(min) {
+  let isInViewport = function(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top <= min &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 // Set sections as active
+  for (i = 1; i < containerCount + 1; i++) {
+    let sectionInFullView = document.getElementById("section" + i);
 
-window.onscroll=function(){myFunction()};
+    window.addEventListener(
+      "scroll",
+      function(event) {
+        if (isInViewport(sectionInFullView)) {
+          sectionInFullView.classList.add("your-active-class");
+        } else {
+          sectionInFullView.classList.remove("your-active-class");
+        }
+      },
+      false
+    );
+  }
+}
+
+//Add sticky header as per review (Used tutorial here: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp)
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+let navbar = document.getElementById("header");
+console.log(navbar)
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+console.log(sticky)
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    sectionswtich("section1",50,100);
-    sectionswtich("section2",100,150);
-    sectionswtich("section3",150,200);
-    sectionswtich("section4",200,250);
-
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
-  // Scroll to section on link click
-  window.onclick=function(){myFunction()};
-  function scrollingFunction(){
-
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-  }
-
-
+}
+sectionSwtich(50);
